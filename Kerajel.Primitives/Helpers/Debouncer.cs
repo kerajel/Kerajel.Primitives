b78094719a4eb83e1b9136eea52c7f77c59c4ff7
Dmitrii Botov <kerajel@gmail.com>
2025-04-30 18:16:06 +0400

    solution clean up

---
+++ Kerajel.Primitives/Helpers/Debouncer.cs
@@ -0,0 +1,108 @@
+﻿namespace Kerajel.Primitives.Helpers;
+
+public class Debouncer<TResult>
+{
+    readonly TimeSpan _debounceInterval;
+    readonly Lock _syncRoot = new();
+    readonly List<TaskCompletionSource<TResult>> _pendingTasks = [];
+
+    Timer? _debounceTimer;
+    CancellationTokenSource? _cancellationTokenSource;
+    Func<Task<TResult>>? _executeAction;
+
+    public Debouncer(TimeSpan interval)
+    {
+        _debounceInterval = interval;
+    }
+
+    public Task<TResult> ExecuteAsync(Func<Task<TResult>> action)
+    {
+        TaskCompletionSource<TResult> taskCompletionSource = new();
+        lock (_syncRoot)
+        {
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource?.Dispose();
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            _executeAction = action;
+            _pendingTasks.Add(taskCompletionSource);
+
+            if (_debounceTimer == null)
+            {
+                _debounceTimer = new Timer(OnTimerElapsed, null, _debounceInterval, Timeout.InfiniteTimeSpan);
+            }
+            else
+            {
+                _debounceTimer.Change(_debounceInterval, Timeout.InfiniteTimeSpan);
+            }
+        }
+        return taskCompletionSource.Task;
+    }
+
+    public void Cancel()
+    {
+        List<TaskCompletionSource<TResult>> pendingTasksCopy;
+        lock (_syncRoot)
+        {
+            _cancellationTokenSource?.Cancel();
+
+            pendingTasksCopy = new List<TaskCompletionSource<TResult>>(_pendingTasks);
+            _pendingTasks.Clear();
+
+            _executeAction = null;
+
+            if (_debounceTimer != null)
+            {
+                _debounceTimer.Dispose();
+                _debounceTimer = null;
+            }
+        }
+
+        foreach (TaskCompletionSource<TResult> taskCompletionSource in pendingTasksCopy)
+        {
+            taskCompletionSource.TrySetCanceled();
+        }
+    }
+
+    async void OnTimerElapsed(object? state)
+    {
+        List<TaskCompletionSource<TResult>> tasksToComplete;
+        Func<Task<TResult>>? actionToExecute;
+        CancellationToken cancellationToken;
+
+        lock (_syncRoot)
+        {
+            tasksToComplete = new List<TaskCompletionSource<TResult>>(_pendingTasks);
+            _pendingTasks.Clear();
+            actionToExecute = _executeAction;
+            _executeAction = null;
+            cancellationToken = _cancellationTokenSource?.Token ?? CancellationToken.None;
+        }
+
+        try
+        {
+            if (actionToExecute != null)
+            {
+                TResult result = await actionToExecute();
+                foreach (TaskCompletionSource<TResult> taskCompletionSource in tasksToComplete)
+                {
+                    taskCompletionSource.TrySetResult(result);
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            foreach (TaskCompletionSource<TResult> taskCompletionSource in tasksToComplete)
+            {
+                taskCompletionSource.TrySetCanceled();
+            }
+        }
+        catch (Exception ex)
+        {
+            foreach (TaskCompletionSource<TResult> taskCompletionSource in tasksToComplete)
+            {
+                taskCompletionSource.TrySetException(ex);
+            }
+        }
+    }
+}
---
+++ Kerajel.Primitives/Helpers/TransactionProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Transactions;
+
+namespace Kerajel.Primitives.Helpers;
+
+public static class TransactionProvider
+{
+    public static readonly TimeSpan MaxTransactionTimeout = TimeSpan.FromMinutes(10);
+
+    static readonly TimeSpan _defaultTransactionTimeout = TimeSpan.FromMinutes(1);
+
+    public static TransactionScope CreateScope(
+        IsolationLevel isolationLevel = IsolationLevel.ReadCommitted,
+        TransactionScopeAsyncFlowOption asyncFlowOption = TransactionScopeAsyncFlowOption.Enabled,
+        TimeSpan transactionTimeout = default)
+    {
+        if (Transaction.Current != null)
+        {
+            isolationLevel = Transaction.Current.IsolationLevel;
+        }
+
+        TransactionOptions transactionOptions = new()
+        {
+            IsolationLevel = isolationLevel,
+            Timeout = transactionTimeout == default
+                ? _defaultTransactionTimeout
+                : transactionTimeout,
+        };
+
+        return new TransactionScope(TransactionScopeOption.Required,
+            transactionOptions, asyncFlowOption);
+    }
+}
